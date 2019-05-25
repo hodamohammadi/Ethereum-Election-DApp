065b48ca2800f47f2491ff0321619e7df1ef543c
Hoda Mohammadi <hoda.muhammadi@td.com>
2019-05-25 17:39:33 -0400

    add vote function to contract

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,22 +1,32 @@
 pragma solidity ^0.5.0;
 
 contract Election {
-	struct Candidate {
+    struct Candidate {
         uint id;
         string name;
         uint voteCount;
     }
 
+    mapping(address => bool) public voters;
     mapping(uint => Candidate) public candidates;
     uint public candidatesCount;
 
-	constructor() public {
-		addCandidate("Candidate 1");
+    constructor() public {
+        addCandidate("Candidate 1");
         addCandidate("Candidate 2");
-	}
+    }
 
-	function addCandidate (string memory _name) private {
+    function addCandidate (string memory _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint _candidateId) public {
+        require(!voters[msg.sender]);
+
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        voters[msg.sender] = true;
+        candidates[_candidateId].voteCount++;
     }
 }
